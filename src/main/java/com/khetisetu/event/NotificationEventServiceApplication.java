30f3ef5859f7d9ee2d274f9fb75c18460f2a86d6
akashkore <akashkore5@gmail.com>
2026-03-25 16:55:38 +0530

    asd

--- src/main/java/com/khetisetu/event/NotificationEventServiceApplication.java
+++ src/main/java/com/khetisetu/event/NotificationEventServiceApplication.java
@@ -4,12 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class })
 @EnableAsync
 @EnableCaching
+@EnableMongoRepositories(basePackages = "com.khetisetu.event.notifications.repository")
 @RestController
 public class NotificationEventServiceApplication {
 	public static void main(String[] args) {
